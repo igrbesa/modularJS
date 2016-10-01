ed36c4cf4e4b8260a0397e3799fc051a723d87a4
Igor Grbeša <igor.m.grbesa@gmail.com>
2016-10-01 19:15:53 +0200

    Revealing Module Pattern Javascript

--- modularJS.js
+++ modularJS.js
@@ -1,82 +1,53 @@
-(function(){
+var people = (function(){
 
-	var people = {
-		peoples: [],
-		init: function(){
-			this.cacheDom();
-			this.bindEvents();
-			this.deleteHover();
-		},
-		cacheDom: function(){
-			this.$input = $('#inputPeopleName');
-			this.$button = $('#addPersonBtn');
-			this.$listPeoples = $('#listpeoples');
-			this.template = $('#people-template').html();
-		},
-		bindEvents: function(){
-			this.$button.on('click', this.addPerson.bind(this));
-			this.$listPeoples.delegate('button', 'click', this.deletePerson.bind(this));
-		},
-		render: function(){
-			var data = {peoples: this.peoples};
-			this.$listPeoples.html(Mustache.render(this.template, data));
-		},
-		addPerson: function(){
-			this.peoples.push({name: this.$input.val()})
-			this.render();
-			this.$input.val('');
-		},
-		deletePerson: function(event){
-			var $remove = $(event.target).closest('a');
-			var i = this.$listPeoples.find('a').index($remove);
-			this.peoples.splice(i, 1);
-			this.render();
-		},
-		deleteHover: function(){
-			this.$listPeoples.delegate('a', 'mouseenter',function(event) {
+		var peoples = [];
+
+		//cache dom elements
+		var $input = $('#inputPeopleName');
+		var $button = $('#addPersonBtn');
+		var $listPeoples = $('#listpeoples');
+		var template = $('#people-template').html();
+
+		//bind events
+		$button.on('click', addPerson);
+		$listPeoples.delegate('button', 'click', deletePerson);
+
+		_render();
+		deleteHover();
+
+		function _render() {
+			var data = {peoples: peoples};
+			$listPeoples.html(Mustache.render(template, data));
+		}
+		function addPerson(value){
+			var name = (typeof value === "string") ? value : $input.val();
+			peoples.push({name: name})
+			_render();
+			$input.val('');
+		}
+		function deletePerson(event){
+			var i;
+			if(typeof event === "number"){
+				i = event;
+			}else{
+				var $remove = $(event.target).closest('a');
+				var i = $listPeoples.find('a').index($remove);
+			}
+			peoples.splice(i, 1);
+			_render();
+		}
+		function deleteHover(){
+			$listPeoples.delegate('a', 'mouseenter',function(event) {
 				$(event.target).find('button').removeClass('hide');
 			});
-			this.$listPeoples.delegate('a', 'mouseleave',function(event) {
+			$listPeoples.delegate('a', 'mouseleave',function(event) {
 				$(event.target).find('button').addClass('hide');
 			});
 		}
 
-	};
-
-	people.init();
+		return {
+			addPerson: addPerson,
+			deletePerson: deletePerson
+		}
 
 })();
-
-
-
-
-
-//var peoples = [];
-//var template = $('#people-template').html();
-//
-////adding new person
-//$('#addPersonBtn').on('click', function(event) {
-//	event.preventDefault();
-//	/* Act on the event */
-//	peoples.push({name: $('#inputPeopleName').val()});
-//	$('#inputPeopleName').val('');
-//	var data = {peoples: peoples};
-//	$('#listpeoples').html(Mustache.render(template, data));
-//});
-//
-//
-//$('#listpeoples').delegate('button', 'click', function(event) {
-//	var $remove = $(event.target).closest('a');
-//	var i = $('#listpeoples').find('a').index($remove);
-//
-//	$remove.remove();
-//	peoples.splice(i, 1);
-//});
-//
-//$('#listpeoples').delegate('a', 'mouseenter', function(event) {
-//	$(event.target).find('button').removeClass('hide');
-//});
-//
-//$('#listpeoples').delegate('a', 'mouseleave', function(event) {
-//	$(event.target).find('button').addClass('hide');
-//});
