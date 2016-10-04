c3a8640320c2d34d3eb15b6749c4ad2f784fd773
Igor Grbeša <igor.m.grbesa@gmail.com>
2016-10-04 21:52:30 +0200

    prototyping with create

--- protottypepattern.js
+++ protottypepattern.js
@@ -5,14 +5,20 @@ var human = {
 	},
 	sayName: function(){
 		console.log(this.name);
+	},
+	create: function(name){
+		var instance = Object.create(this);
+		instance.name = name;
+		return instance;
 	}
 }
 
+/* this is replaced with create function
 var musician = Object.create(human);
 musician.playInstument = function(){
 	console.log("playing instrumnet: " + this.instrumnet);
 }
-
+*/
 var will = Object.create(musician);
 will.name = "Will";
 will.instrumnet = "Drums";
